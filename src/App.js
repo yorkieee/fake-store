9df2f04657f4481b34109e7e127f92fea29eaf8f
AnastasiaNaa <104014003+AnastasiaNaa@users.noreply.github.com>
2022-10-07 14:25:12 +0200

    App fetches data now

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { ShopList } from "./ShopList";
 import { ProductDisplay } from "./ProductDisplay";
 import { ErrorPage } from "./ErrorPage";
+import { useFetchData } from "./hooks/useFetchData";
 
 function App() {
+  const [itemsData, setItemsData] = useState([]);
+  useFetchData(setItemsData);
+
   return (
     <Router>
       <nav>
         <Link to="./ShopList"> Home </Link>
       </nav>
       <Routes>
-        <Route path="/ShopList" element={<ShopList />} />
+        <Route path="/ShopList" element={<ShopList itemsData={itemsData} />} />
         <Route
           path="/ProductDisplay"
-          element={<ProductDisplay itemsData={null} />}
+          element={<ProductDisplay itemsData={itemsData} />}
         />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
