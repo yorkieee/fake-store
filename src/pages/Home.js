165bc77430e9f25e9b4cedac76c003ec74638f1a
Anastasiia Nahirniak <anastasiianahirniak@MBPvonAastasiia.fritz.box>
2022-10-10 18:24:30 +0200

    using context in home and login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
-import React from "react";
-import Grid from "@mui/material/Grid";
+import React, { useContext } from 'react';
+import Grid from '@mui/material/Grid';
 
-import { ItemCard } from "../ItemCard";
-import { Container } from "@mui/system";
+import { ItemCard } from '../ItemCard';
+import { Container } from '@mui/system';
+
+import { LoginContext } from '../App';
 
 export const Home = ({ itemsData }) => {
+  const [loginState] = useContext(LoginContext);
+
   return (
     <div>
       <Container>
+        <p>{`User is loged in ${loginState}`}</p>
         <Grid container spacing={4}>
           {itemsData.map((item, id) => (
             <Grid item key={id} xs={12} md={3} lg={4}>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,17 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { LoginContext } from '../App';
 export const Login = () => {
-  return <h1>Login Page</h1>;
+  const [loginState, setLoginState] = useContext(LoginContext);
+
+  const onLoginClick = () => {
+    setLoginState(!loginState);
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <h2>{`Logged in: ${loginState}`}</h2>
+      <button onClick={onLoginClick}>Log In</button>
+    </div>
+  );
 };
