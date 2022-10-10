354aba8ad993d9f94d50b5e7edcc339d6bdb97b5
Anastasiia Nahirniak <anastasiianahirniak@MBPvonAastasiia.fritz.box>
2022-10-10 18:23:59 +0200

    created login context

--- src/App.js
+++ src/App.js
@@ -8,25 +8,31 @@ import { ErrorPage } from './pages/ErrorPage';
 import { ProductPage } from './pages/ProductPage';
 import { Login } from './pages/Login';
 
+export const LoginContext = React.createContext([false, () => {}]);
+
 const App = () => {
   const [itemsData, setItemsData] = useState([]);
   useFetchData(setItemsData);
 
+  const LoginStateControl = useState(false);
+
   return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home itemsData={itemsData} />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/ProductDisplay">
-          <Route path=":id" element={<ProductPage itemsData={itemsData} />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <LoginContext.Provider value={LoginStateControl}>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home itemsData={itemsData} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/ProductDisplay">
+            <Route path=":id" element={<ProductPage itemsData={itemsData} />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </LoginContext.Provider>
   );
 };
 
